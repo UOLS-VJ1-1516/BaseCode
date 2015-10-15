4a181a235c9713f713aa526dd84a2db16bd032d7
Roger De Moreta <rogermoreta@gmail.com>
2015-10-15 21:47:37 +0200

    Ya se mueven en pantalla completa

--- main.cpp
+++ main.cpp
@@ -2,9 +2,12 @@
 
 SDL_Window* g_pWindow = 0;
 SDL_Renderer* g_pRenderer = 0;
+SDL_Event event;
 
 int main(int argc, char* args[])
 {
+	int* pixelesDeAncho = new int(640);
+	int* pixelesDeAlto = new int(480);
 	// initialize SDL
 	if(SDL_Init(SDL_INIT_EVERYTHING) >= 0)
 	{
@@ -12,7 +15,9 @@ int main(int argc, char* args[])
 		g_pWindow = SDL_CreateWindow("Videjuegos 1 - bachelor",
 		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
 		640, 480,
-		SDL_WINDOW_SHOWN);
+			SDL_WINDOW_FULLSCREEN_DESKTOP);
+
+		SDL_GetWindowSize(g_pWindow, pixelesDeAncho, pixelesDeAlto);
 
 		// if the window creation succeeded create our renderer
 		if(g_pWindow != 0)
@@ -28,51 +33,58 @@ int main(int argc, char* args[])
 	// everything succeeded lets draw the window
 	// set to black // This function expects Red, Green, Blue and
 	// Alpha as color values
-	SDL_SetRenderDrawColor(g_pRenderer, 0, 10, 40, 255);
+
+	Uint32 start = SDL_GetTicks();
+	Uint32 lastTime = SDL_GetTicks();
+	int tiempo = 0;
+
+	while (true) {
+
+		SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
 
 
-	// clear the window to black
-	SDL_RenderClear(g_pRenderer);
+		// clear the window to black
+		SDL_RenderClear(g_pRenderer);
 
-	int numeroDivisiones = 6;
-	int numeroPixelesDivision = 640 / 6;
-	int pixel = 0;
-	int divisionActual = 0;
-	for (pixel = 0; pixel < numeroPixelesDivision*(divisionActual+1); pixel++) {
-		SDL_SetRenderDrawColor(g_pRenderer, 255, (Uint8)(255*(pixel- numeroPixelesDivision*divisionActual)/ numeroPixelesDivision), 0, 255); //Rojo a Amarillo
-		SDL_RenderDrawLine(g_pRenderer, pixel, 0, pixel, 480);
-	}
-	divisionActual++;
-	for (; pixel < numeroPixelesDivision*(divisionActual + 1); pixel++) {
-		SDL_SetRenderDrawColor(g_pRenderer, 255-(Uint8)(255 * (pixel - numeroPixelesDivision*divisionActual) / numeroPixelesDivision), 255, 0, 255); //Amarillo a Verde
-		SDL_RenderDrawLine(g_pRenderer, pixel, 0, pixel, 480);
-	}
-	divisionActual++;
-	for (; pixel < numeroPixelesDivision*(divisionActual + 1); pixel++) {
-		SDL_SetRenderDrawColor(g_pRenderer, 0, 255, (Uint8)(255 * (pixel - numeroPixelesDivision*divisionActual) / numeroPixelesDivision), 255); //Verde a Cyan
-		SDL_RenderDrawLine(g_pRenderer, pixel, 0, pixel, 480);
-	}
-	divisionActual++;
-	for (; pixel < numeroPixelesDivision*(divisionActual + 1); pixel++) {
-		SDL_SetRenderDrawColor(g_pRenderer, 0, 255 - (Uint8)(255 * (pixel - numeroPixelesDivision*divisionActual) / numeroPixelesDivision), 255, 255); //Cyan a Blue
-		SDL_RenderDrawLine(g_pRenderer, pixel, 0, pixel, 480);
-	}
-	divisionActual++;
-	for (; pixel < numeroPixelesDivision*(divisionActual + 1); pixel++) {
-		SDL_SetRenderDrawColor(g_pRenderer, (Uint8)(255 * (pixel - numeroPixelesDivision*divisionActual) / numeroPixelesDivision), 0, 255, 255); //Blue a Lila
-		SDL_RenderDrawLine(g_pRenderer, pixel, 0, pixel, 480);
-	}
-	divisionActual++;
-	for (; pixel < numeroPixelesDivision*(divisionActual + 1); pixel++) {
-		SDL_SetRenderDrawColor(g_pRenderer, 255, 0, 255- (Uint8)(255 * (pixel - numeroPixelesDivision*divisionActual) / numeroPixelesDivision), 255); //Lila a Rojo
-		SDL_RenderDrawLine(g_pRenderer, pixel, 0, pixel, 480);
-	}
+		int numeroDivisiones = 6;
+		int numeroPixelesDivision = *pixelesDeAncho / 6;
+		int pixel = 0;
+		int divisionActual = 0;
+		tiempo += SDL_GetTicks()-lastTime;
+		lastTime = SDL_GetTicks();
+		for (pixel = 0; pixel < numeroPixelesDivision*(divisionActual + 1); pixel++) {
+			SDL_SetRenderDrawColor(g_pRenderer, 255, (Uint8)(255 * (pixel - numeroPixelesDivision*divisionActual) / numeroPixelesDivision), 0, 255); //Rojo a Amarillo
+			SDL_RenderDrawLine(g_pRenderer, (pixel + tiempo) % *pixelesDeAncho, 0, (pixel + tiempo) % *pixelesDeAncho, *pixelesDeAlto);
+		}
+		divisionActual++;
+		for (; pixel < numeroPixelesDivision*(divisionActual + 1); pixel++) {
+			SDL_SetRenderDrawColor(g_pRenderer, 255 - (Uint8)(255 * (pixel - numeroPixelesDivision*divisionActual) / numeroPixelesDivision), 255, 0, 255); //Amarillo a Verde
+			SDL_RenderDrawLine(g_pRenderer, (pixel + tiempo) % *pixelesDeAncho, 0, (pixel + tiempo) % *pixelesDeAncho, *pixelesDeAlto);
+		}
+		divisionActual++;
+		for (; pixel < numeroPixelesDivision*(divisionActual + 1); pixel++) {
+			SDL_SetRenderDrawColor(g_pRenderer, 0, 255, (Uint8)(255 * (pixel - numeroPixelesDivision*divisionActual) / numeroPixelesDivision), 255); //Verde a Cyan
+			SDL_RenderDrawLine(g_pRenderer, (pixel + tiempo) % *pixelesDeAncho, 0, (pixel + tiempo) % *pixelesDeAncho, *pixelesDeAlto);
+		}
+		divisionActual++;
+		for (; pixel < numeroPixelesDivision*(divisionActual + 1); pixel++) {
+			SDL_SetRenderDrawColor(g_pRenderer, 0, 255 - (Uint8)(255 * (pixel - numeroPixelesDivision*divisionActual) / numeroPixelesDivision), 255, 255); //Cyan a Blue
+			SDL_RenderDrawLine(g_pRenderer, (pixel + tiempo) % *pixelesDeAncho, 0, (pixel + tiempo) % *pixelesDeAncho, *pixelesDeAlto);
+		}
+		divisionActual++;
+		for (; pixel < numeroPixelesDivision*(divisionActual + 1); pixel++) {
+			SDL_SetRenderDrawColor(g_pRenderer, (Uint8)(255 * (pixel - numeroPixelesDivision*divisionActual) / numeroPixelesDivision), 0, 255, 255); //Blue a Lila
+			SDL_RenderDrawLine(g_pRenderer, (pixel + tiempo) % *pixelesDeAncho, 0, (pixel + tiempo) % *pixelesDeAncho, *pixelesDeAlto);
+		}
+		divisionActual++;
+		for (; pixel < numeroPixelesDivision*(divisionActual + 1); pixel++) {
+			SDL_SetRenderDrawColor(g_pRenderer, 255, 0, 255 - (Uint8)(255 * (pixel - numeroPixelesDivision*divisionActual) / numeroPixelesDivision), 255); //Lila a Rojo
+			SDL_RenderDrawLine(g_pRenderer, (pixel + tiempo) % *pixelesDeAncho, 0, (pixel + tiempo) % *pixelesDeAncho, *pixelesDeAlto);
+		}
 
-	// show the window
-	SDL_RenderPresent(g_pRenderer);
-
-	// set a delay before quitting
-	SDL_Delay(5000);
+		// show the window
+		SDL_RenderPresent(g_pRenderer);
+	}
 
 	// clean up SDL
 	SDL_Quit();
