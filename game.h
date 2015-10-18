4602c4d0ad4f80df7ac456e4ab8f6e16c1be2b1b
fhomedes <fhomedes1991@gmail.com>
2015-10-18 18:40:37 +0200

    version final

---
+++ game.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "SDL.h"
+class Game
+{
+	SDL_Window* _window = 0;
+	SDL_Renderer* _renderer = 0;
+	bool _isRunning;
+	SDL_Event event;
+	int r, g, b;
+
+public:
+	Game();
+	~Game();
+	bool init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+	void render();
+	void update();
+	void handleEvents();
+	void clean();
+	bool isRunning();
+};
+
--- main.cpp
+++ main.cpp
@@ -1,46 +1,24 @@
-#include "SDL.h"
+#include "game.h"
 
-SDL_Window* g_pWindow = 0;
-SDL_Renderer* g_pRenderer = 0;
+
 
 int main(int argc, char* args[])
 {
-	// initialize SDL
-	if(SDL_Init(SDL_INIT_EVERYTHING) >= 0)
-	{
-		// if succeeded create our window
-		g_pWindow = SDL_CreateWindow("Videjuegos 1 - bachelor",
-		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-		640, 480,
-		SDL_WINDOW_SHOWN);
+	bool ok;
+	Game game;
+	
+	ok = game.init("", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, false);
 
-		// if the window creation succeeded create our renderer
-		if(g_pWindow != 0)
+	if (ok)
+	{
+		while (game.isRunning())
 		{
-			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+			game.handleEvents();
+			game.update();
+			game.render();
 		}
+		game.clean();
 	}
-	else
-	{
-		return 1; // sdl could not initialize
-	}
-
-	// everything succeeded lets draw the window
-	// set to black // This function expects Red, Green, Blue and
-	// Alpha as color values
-	SDL_SetRenderDrawColor(g_pRenderer, 0, 10, 40, 255);
-
-	// clear the window to black
-	SDL_RenderClear(g_pRenderer);
-
-	// show the window
-	SDL_RenderPresent(g_pRenderer);
-
-	// set a delay before quitting
-	SDL_Delay(5000);
-
-	// clean up SDL
-	SDL_Quit();
 
 	return 0;
 }
