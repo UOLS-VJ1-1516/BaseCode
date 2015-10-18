10fff6cc6d0792aa0bb5d4432049728628ef0cd5
MaarcCF <dac6928@gmail.com>
2015-10-18 21:37:29 +0200

    finished

---
+++ game.h
@@ -0,0 +1,19 @@
+#include "SDL.h"
+class game
+{
+	SDL_Window* g_pWindow = 0;
+	SDL_Renderer* g_pRenderer = 0;
+	bool running = true;
+
+public:
+	game();
+	~game();
+
+	bool init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+	void render(int r, int g, int b);
+	void update();
+	void handleEvents(SDL_Event event);
+	void clean();
+	bool isRunning();
+};
+
