cda1018bf381416078996311cbc426e73eb103d7
Cordobes91 <victorcordobes91@gmail.com>
2015-10-18 16:58:38 +0200

    Primera revision del codigo, creacion del game cpp y game.h

---
+++ game.cpp
@@ -0,0 +1,88 @@
+#include "game.h"
+
+
+
+
+game::game()
+{
+}
+
+
+game::~game()
+{
+}
+
+
+//ahora añadimos las funciones declaradas en game.h y las rellenamos
+bool game::init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen) {
+	//en este caso añadimos el codigo que estaba incluido en el main.cpp
+	// initialize SDL
+	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+	{
+		// if succeeded create our window
+		g_pWindow = SDL_CreateWindow("Videjuegos 1 - Victor Cordobés",
+			SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+			640, 480,
+			SDL_WINDOW_SHOWN);
+
+		// if the window creation succeeded create our renderer
+		if (g_pWindow != 0)
+		{
+			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+		}
+	}
+	else
+	{
+		return 1; // sdl could not initialize
+	}
+
+};
+
+void game::render() {
+
+
+	SDL_SetRenderDrawColor(g_pRenderer, 0, 10, 40, 255);
+
+
+	// clear the window to black
+	SDL_RenderClear(g_pRenderer);
+
+	// show the window
+	SDL_RenderPresent(g_pRenderer);
+
+	
+};
+
+
+void game::update() {
+
+
+	SDL_Delay(9000);
+
+};
+
+
+void game::handleEvents() {
+
+
+};
+
+
+void game::clean() {
+
+
+	// clean up SDL
+	SDL_Quit();
+
+};
+
+
+bool game::isRunning() {
+
+	//return running;
+
+
+};
+
+
+
---
+++ game.h
@@ -0,0 +1,23 @@
+#include "SDL.h"
+
+class game
+{
+
+	//declaramos las variables tal y como estaban en el main.cpp
+	SDL_Window* g_pWindow = 0;
+	SDL_Renderer* g_pRenderer = 0;
+
+
+
+public:
+	game();
+	~game();
+	bool init( const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+	void render();
+	void update();
+	void handleEvents();
+	void clean();
+	bool isRunning();
+
+};
+
