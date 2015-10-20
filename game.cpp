61486ffef9115a361c8fb8f4ab673d64bee8a90e
Raúl Álvarez San Jose <raul.alsan@gmail.com>
2015-10-20 18:40:54 +0200

    Programa terminadoooo

---
+++ game.cpp
@@ -0,0 +1,102 @@
+#include "SDL.h"
+#include "game.h"
+#include "time.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+SDL_Window* g_lWindow = 0;
+SDL_Renderer* g_lRenderer = 0;
+
+Game::Game() {  //Constructor
+
+}
+Game::~Game() {
+
+}
+
+
+
+bool Game::init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen) {
+
+
+
+	// initialize SDL
+	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+	{
+
+		// if succeeded create our window
+		g_lWindow = SDL_CreateWindow(title, xpos, ypos, width, height, fullscreen);
+
+		// if the window creation succeeded create our renderer
+		if (g_lWindow != 0)
+		{
+			g_lRenderer = SDL_CreateRenderer(g_lWindow, -1, 0);
+		}
+		running = true;
+	}
+	else
+	{
+		return 1; // sdl could not initialize
+	}
+
+	// everything succeeded lets draw the window
+	// set to black // This function expects Red, Green, Blue and
+	// Alpha as color values
+	// Especifiquem el color amb que pintarem
+
+
+	return true;
+}
+
+void Game::update() {  //Actualitzara el colors
+	int x = rand();
+	int y = rand();
+	int z = rand();
+	int h = rand();
+	SDL_SetRenderDrawColor(g_lRenderer, x, y, z, h);
+
+}
+void Game::render() { //Actualitzara el buffer i mostrara per pantalla
+	update();
+	SDL_RenderClear(g_lRenderer);
+	SDL_RenderPresent(g_lRenderer);
+
+
+}
+
+
+void Game::clean() {
+	SDL_DestroyWindow(g_lWindow);
+	SDL_DestroyRenderer(g_lRenderer);
+	SDL_Quit();
+
+
+
+
+}
+bool Game::isRunning() { 
+return running; 
+}
+
+
+int Game::handleEvents() {
+	SDL_Event event;
+	while (SDL_PollEvent(&event)) {
+		running = true;
+		if (event.type == SDL_KEYUP) {
+			running = false;
+		}
+	}
+	return 1;
+}
+
+
+
+
+
+
+
+
+
+
--- main.cpp
+++ main.cpp
@@ -1,46 +1,23 @@
-#include "SDL.h"
+#include "game.h"
 
-SDL_Window* g_pWindow = 0;
-SDL_Renderer* g_pRenderer = 0;
+Game joc;
 
 int main(int argc, char* args[])
 {
-	// initialize SDL
-	if(SDL_Init(SDL_INIT_EVERYTHING) >= 0)
-	{
-		// if succeeded create our window
-		g_pWindow = SDL_CreateWindow("Videjuegos 1 - bachelor",
-		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-		640, 480,
-		SDL_WINDOW_SHOWN);
 
-		// if the window creation succeeded create our renderer
-		if(g_pWindow != 0)
-		{
-			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-		}
-	}
-	else
-	{
-		return 1; // sdl could not initialize
+	joc.init("Videojocs1 - bachelor", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, true); //SDL_WINDOW_SHOWN
+
+	while (joc.isRunning()) { 
+		joc.handleEvents();	
+		joc.update();
+		joc.render();
+		SDL_Delay(550); 
 	}
 
-	// everything succeeded lets draw the window
-	// set to black // This function expects Red, Green, Blue and
-	// Alpha as color values
-	SDL_SetRenderDrawColor(g_pRenderer, 0, 10, 40, 255);
-
-	// clear the window to black
-	SDL_RenderClear(g_pRenderer);
-
-	// show the window
-	SDL_RenderPresent(g_pRenderer);
-
-	// set a delay before quitting
-	SDL_Delay(5000);
-
-	// clean up SDL
-	SDL_Quit();
+	joc.clean();
 
 	return 0;
 }
+
+
+
