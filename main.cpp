76c00d47cd6155d68d8c0be49bbbef771381b58f
Cordobes91 <victorcordobes91@gmail.com>
2015-11-03 15:10:12 +0100

    Entrega deVictor Cordobés ls31253

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char* args[])
 	int b = 0;
 	int c = 0;
 
-	game::Instance()->init("Videojuegos1", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1600, 800 , false);
+	game::Instance()->init("Videojuegos1 Ejercicio3", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1600, 800 , false);
 
 	while (game::Instance()->isRunning())
 	{
@@ -26,3 +26,6 @@ int main(int argc, char* args[])
 	game::Instance()->clean();
 	return 0;
 }
+
+
+//Entrega deVictor Cordobés ls31253.zip
