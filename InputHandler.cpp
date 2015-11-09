93ef578e9af543066637ede7dcf06cae47402cb3
ls30726 <andres.prog.dev@gmail.com>
2015-11-10 00:19:37 +0100

    ajustes

--- InputHandler.cpp
+++ InputHandler.cpp
@@ -1,9 +1,5 @@
 #include "InputHandler.h"
-#include <SDL.h>
 
-const int SIZE_WINDOW_HEIGHT = 800;
-const int SIZE_WINDOW_WIDTH = 800;
-const int FIXED_TIME = 35;
 InputHandler * InputHandler::s_pInstance = 0;
 InputHandler::InputHandler()
 {
